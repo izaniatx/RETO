349c2aa51f18a476ad20bd6dc1b2257131acbf28
Izani <iachega06github@gmail.com>
2026-01-07 14:20:23 +0100

    landing page termianda

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -1,123 +1,127 @@
 import MainLayout from "../layouts/MainLayout";
 import Aro from "../assets/Aro.png";
-import "./css/landingpage.css";
-import LoginModal from "../components/LoginModal";
-import Marquee from "react-fast-marquee";
 import bmw from "../assets/logos/bmw.png";
-import hyundai from "../assets/logos/hyundai.gif";
-import mercedes from "../assets/logos/mercedes.png";
-import audi from "../assets/logos/audi.png";
-import volkswagen from "../assets/logos/volkswagen.png";
-import ford from "../assets/logos/ford.png";
 import mazda from "../assets/logos/mazda.png";
-import nissan from "../assets/logos/nissan.png";
-import honda from "../assets/logos/honda.png";
-import seat from "../assets/logos/seat.png";
-import { div } from "motion/react-client";
-
+import c1 from "../assets/coches/coche.png";
+import c2 from "../assets/coches/coche2.png";
+import c3 from "../assets/coches/coche3.png";
+import "./css/landingpage.css";
 
 function landingpage() {
-  return ( 
-    <div className="landing-bg"> 
-      <MainLayout>   
-          <div className="container col-xxl-12 px-4 py-5" style={{ color: "white"}}>
-            <div style={{ marginBottom: "300px", marginTop: "200px" }}>
-              <div>
-                <h1 className="display-5 fw-bold lh-1 mb-3 text-center">Bienvenid@ a Aro Automoción</h1>
-                <p className="lead text-center">¡Descubre las mejores ofertas en coches!</p>
-              </div>
-              <div className="d-grid gap-2 d-md-flex justify-content-md-center">
-                    <button type="button" id="btn-1" className="btn btn-primary btn-lg px-4 me-md-2"
-                      data-bs-toggle="modal" data-bs-target="#loginModal"
-                    >Iniciar sesión</button>
-                    <LoginModal />
-                    <button type="button" id="btn-2" className="btn btn-outline-secondary btn-lg px-4"
-                      onClick={() => window.location.href = '/RETO/#/inicio'}
-                    >Más información</button>
-              </div>
-            </div>
-            
-            <div className="row flex-lg-row-reverse align-items-center g-5 py-5" id="about-us" >
-              <div className="col-10 col-sm-6 col-lg-6">
-                <img src={Aro} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="500" height="300" loading="lazy" style={{ borderRadius: "10px" }}/>
-              </div>
-              <div className="col-lg-6">
-                <h1 className="display-5 fw-bold lh-1 mb-3">Sobre Nosotros</h1>
-                <p className="lead" style={{ textAlign: 'justify' }}>Aro Automoción es una red internacional de concesionarios dedicada a ofrecer 
-                  soluciones integrales de movilidad con los más altos estándares de calidad, 
-                  confianza y servicio. Con presencia en varios países, nuestra empresa se ha 
-                  consolidado como un referente en el sector automotriz gracias a un modelo de 
-                  atención centrado en las personas y a una amplia gama de vehículos que se adapta 
-                  a las necesidades de cada cliente.
-                </p>  
-              </div>
-            </div>
-            
-            
-            <Marquee gradient={false} speed={100} pauseOnHover={true} style={{ marginTop: "100px", marginBottom: "50px", fontSize: "24px", fontWeight: "bold" }}>
-              ¡Encuentra tu coche ideal con Aro Automoción! &nbsp; • &nbsp; Amplia selección de vehículos &nbsp; • &nbsp; Financiación a medida &nbsp; • &nbsp; Servicio postventa excepcional &nbsp; • &nbsp; ¡Visítanos hoy mismo! &nbsp; • &nbsp;
-            </Marquee> 
-          </div>
-          <div className="container-card">
-            <h2 className="mb-5">¿Por qué elegir Aro Automoción?</h2>
-            <div className="container">
-              <div className="row justify-content-center">
-                
-                {/* Tarjeta 1 */}
-                <div className="col-md-4 mb-4">
-                  <div className="card h-100 text-center bg-dark text-white border-light">
-                    <div className="card-body">
-                      <h5 className="card-title">Vehículos de calidad</h5>
-                      <p className="card-text">
-                        Solo trabajamos con coches revisados y certificados para que tengas la máxima confianza en tu compra.
-                      </p>
-                    </div>
-                  </div>
-                </div>
+  return (
+    <div className="landing">
+      <MainLayout>
+        <div className="container col-xxl-12 px-4 py-5 d-flex flex-column align-items-center justify-content-center" style={{ minHeight: "80vh" }}>
 
-                {/* Tarjeta 2 */}
-                <div className="col-md-4 mb-4">
-                  <div className="card h-100 text-center bg-dark text-white border-light">
-                    <div className="card-body">
-                      <h5 className="card-title">Financiación a medida</h5>
-                      <p className="card-text">
-                        Te ayudamos a encontrar la mejor opción de financiación según tus necesidades y presupuesto.
-                      </p>
-                    </div>
-                  </div>
-                </div>
+          <div style={{ color: "white" }}>
+            <h1 className="display-5 fw-bold lh-1 mb-3 text-center" >Bienvenid@ a Aro Automocion</h1>
+            <p className="text-center">¡Descubre las mejores ofertas en coches!</p>
 
-                {/* Tarjeta 3 */}
-                <div className="col-md-4 mb-4">
-                  <div className="card h-100 text-center bg-dark text-white border-light">
-                    <div className="card-body">
-                      <h5 className="card-title">Servicio postventa</h5>
-                      <p className="card-text">
-                        Nuestro equipo de atención al cliente está disponible para ayudarte incluso después de la compra.
-                      </p>
-                    </div>
-                  </div>
-                </div>
+          </div>
+          <div className="d-grid gap-2 d-md-flex justify-content-md-center">
+            <button type="button" id="btn-1" className="btn btn-primary btn-lg px-4 me-md-2">Iniciar sesión</button>
+            <button type="button" id="btn-2" className="btn btn-outline-secondary btn-lg px-4">Más información</button>
+          </div>
+
+
+        </div>
+        <div id="landingPage" style={{ minHeight: "100vh" }}>
+
+
+          <div className="contacto">
+            <div className="row flex g-5 py-5 rounded  justify-content-around align-items-center" style={{ maxWidth: "100%", color: "white" }}>
+              <div className="col-12 col-md-8 col-lg-6">
+
+                <h1 className="display-5 fw-bold lh-1 mb-3">SOBRE NOSOTROS</h1>
+                <p className="lead">   Aro Automoción es una red internacional de concesionarios dedicada a
+                  ofrecer soluciones integrales de movilidad con los más altos estándares de calidad, confianza
+                  y servicio. Con presencia en varios países, nuestra empresa se ha consolidado como un referente
+                  en el sector automotriz gracias a un modelo de atención centrado en las personas
+                  y a una amplia gama de vehículos que se adapta a las necesidades de cada cliente.</p>
               </div>
             </div>
+
+            <h1>OFERTAS</h1>
+
+            <div className="d-flex flex-column flex-lg-row justify-content-around align-items-center gap-4"
+              style={{ minHeight: "50vh" }}>
+
+              <div className="card shadow p-3 mb-4 rounded cartasOfertas"
+                style={{ width: "100%", maxWidth: "27rem", height: "22rem" }}>
+                <div className="card-body">
+                  <img src={c1} className="rounded w-100 mb-2" style={{ height: "60%", objectFit: "cover" }} />
+                  <h5 className="card-title">TITULO</h5>
+                  <p className="card-text">Parrafo prueba</p>
+                </div>
+              </div>
+
+              <div className="card shadow p-3 mb-4 rounded cartasOfertas"
+                style={{ width: "100%", maxWidth: "27rem", height: "22rem" }}>
+                <div className="card-body">
+                  <img src={c2} className="rounded w-100 mb-2" style={{ height: "60%", objectFit: "cover" }} />
+                  <h5 className="card-title">TITULO</h5>
+                  <p className="card-text">Parrafo prueba</p>
+                </div>
+              </div>
+
+              <div className="card shadow p-3 mb-4 rounded cartasOfertas"
+                style={{ width: "100%", maxWidth: "27rem", height: "22rem" }}>
+                <div className="card-body">
+                  <img src={c3} className="rounded w-100 mb-2" style={{ height: "60%", objectFit: "cover" }} />
+                  <h5 className="card-title">TITULO</h5>
+                  <p className="card-text">Parrafo prueba</p>
+                </div>
+              </div>
+
+            </div>
           </div>
-          <div id="cuerpo">
-              <Marquee gradient={true} gradientColor= "rgba(0, 0, 0, 0.85)" speed={100} pauseOnHover={true} style={{ marginTop: "100px", marginBottom: "50px", fontSize: "24px", fontWeight: "bold" }}>
-                &nbsp; <a href=""><img src={bmw} alt="bmw" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={mercedes} alt="mercedes" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={audi} alt="audi" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={volkswagen} alt="volkswagen" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={hyundai} alt="hyundai" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={mazda} alt="mazda" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={ford} alt="ford" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={nissan} alt="nissan" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={seat} alt="seat" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-                &nbsp; <a href="#"><img src={honda} alt="honda" style={{ height: 200, verticalAlign: "middle" }} /></a> &nbsp; 
-              </Marquee> 
+
+
+
+          <div>
+            <div className="d-flex flex-column flex-lg-row justify-content-center align-items-center m-4 gap-4">
+
+              {/* Card del formulario */}
+              <div className="card shadow p-4"
+                style={{ width: "100%", maxWidth: "35rem", backgroundColor: "#292929ff", color: "white" }}>
+                <h2 className="mb-3">CONTACTO</h2>
+
+                <form>
+                  <div className="mb-3">
+                    <label className="form-label">Nombre</label>
+                    <input type="text" className="form-control" placeholder="Tu nombre" required />
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">Email</label>
+                    <input type="email" className="form-control" placeholder="tuemail@ejemplo.com" required />
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">Mensaje</label>
+                    <textarea className="form-control" rows="5"
+                      placeholder="Escribe tu mensaje aquí..." required />
+                  </div>
+
+                  <button type="submit" className="btn btn-primary w-100 boton">Enviar</button>
+                </form>
+              </div>
+
+              {/* Imagen */}
+              <div className="d-flex justify-content-center align-items-center">
+                <img src={Aro}
+                  alt="Aro"
+                  className="rounded w-100"
+                  style={{ maxWidth: "500px", height: "500px" }} />
+              </div>
+
+            </div>
+
+
           </div>
+        </div>
       </MainLayout>
     </div>
-
   );
 }
 
